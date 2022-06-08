e91dad01000e7b631f3cd3645aedc2c39ccf9421
abdulaziz1999 <azizmentor96@gmail.com>
2022-06-08 17:20:26 +0700

    update controller home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,9 +2,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('M_mahasiswa');
+	}
 
 	public function index()
 	{
-		$this->load->view('page/view_home');
+		$data['dataJoin'] 		= $this->M_mahasiswa->getJoinProdi();
+		$data['countMahasiswa'] = $this->M_mahasiswa->countMahasiswa();
+		$data['countDosen'] 	= $this->M_mahasiswa->countDosen();
+		$data['countProdi'] 	= $this->M_mahasiswa->countProdi();
+
+		$this->load->view('landing/home',$data);
 	}
 }
